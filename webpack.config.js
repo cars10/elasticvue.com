bf5ec33b493f89e240c948f4070117e523bfaf4a
Carsten König <carstenkoenig92@gmail.com>
2018-06-16 19:42:12 +0200

    adds webpack etc

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const dev = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+    output: {
+        filename: dev ? '[name].js' : '[name].[chunkhash].js'
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: false
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg)$/,
+                use: ["file-loader"]
+            },
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: 'html-loader',
+                    options: {
+                        minimize: true,
+                        removeComments: true,
+                        collapseWhitespace: true
+                    }
+                }]
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    dev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: dev ? '[name].css' : 'style.[hash].css',
+            chunkFilename: dev ? '[id].css' : '[id].[hash].css',
+        })
+    ]
+}
