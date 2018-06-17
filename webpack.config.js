d5b802739ae04167af45700c544273d6873a09a9
Carsten König <carstenkoenig92@gmail.com>
2018-06-17 16:46:01 +0200

    gzip assets

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin")
+
 const dev = process.env.NODE_ENV !== 'production'
 
 module.exports = {
@@ -52,6 +54,9 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: dev ? '[name].css' : 'style.[hash].css',
             chunkFilename: dev ? '[id].css' : '[id].[hash].css',
+        }),
+        new CompressionPlugin({
+            test: /\.(js|css)/
         })
     ]
 }
