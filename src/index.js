0cd5fc179d3841391c21b96011f90feaa9838818
Carsten König <carstenkoenig92@gmail.com>
2020-01-02 08:12:03 +0100

    fix sw path

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './assets/stylesheets/style.scss'
 if (process.env.NODE_ENV === 'production') {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/serviceWorker.js').then(() => {
+      navigator.serviceWorker.register('/service-worker.js').then(() => {
       }).catch(registrationError => {
         console.log('SW registration failed: ', registrationError)
       })
