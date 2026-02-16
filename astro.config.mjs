acb8856fa6602ef5bdc1e310a5a0f666f495aea2
Carsten König <carstenkoenig92@gmail.com>
2026-02-16 19:58:56 +0100

    improve font rendering even more

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   cacheDir: '.astro',
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: 'Inter',
+        cssVariable: '--font-inter',
+      },
+    ],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
