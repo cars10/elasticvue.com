38d2bf0e101dc30a3637d03a6e5da25568378631
Carsten König <carstenkoenig92@gmail.com>
2026-09-20 15:18:28 +0200

    update astro v7

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@ import { defineConfig, fontProviders } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.fontsource(),
-        name: 'Inter',
-        cssVariable: '--font-inter',
-      },
-    ],
-  },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Inter',
+      cssVariable: '--font-inter',
+    },
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
